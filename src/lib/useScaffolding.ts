66ed48069d5af26467dd23b6d8509700ec39d1b0
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-05-07 17:21:06 +0200

    :technologist: Better UI for scaffolding.

--- src/lib/useScaffolding.ts
+++ src/lib/useScaffolding.ts
@@ -1,24 +1,71 @@
 #!/usr/bin/env node
 
 import {
+  useConfirm,
   useCurrentPath,
   useGenerator,
+  useDisplayJson,
   usePackageStubsPath,
   usePrompt,
+  useSentence,
 } from "@henrotaym/scaffolding-utils";
 
 const useStubsPath = usePackageStubsPath("@henrotaymcorp/vue-boilerplate");
 
 const useScaffolding = () => {
-  const generator = useGenerator({
-    organizationName: usePrompt("Organization name"),
-    packageName: usePrompt("Package name"),
-    description: usePrompt("Package description"),
-    authorName: usePrompt("Author full name (first_name last_name)"),
-    authorEmail: usePrompt("Author email"),
-  });
+  const folder = usePrompt("Folder location [.]", ".");
+  const location = useCurrentPath(folder);
+  const lastFolderLocationName = location.split("/").slice(-1)[0];
 
-  generator.copy(useStubsPath(), useCurrentPath());
+  const organizationName = usePrompt(
+    "Organization name [deegital]",
+    "deegital"
+  );
+
+  const packageName = usePrompt(
+    `Package name [${lastFolderLocationName}]`,
+    lastFolderLocationName
+  );
+
+  const description = usePrompt(
+    `Package description [${lastFolderLocationName}]`,
+    lastFolderLocationName
+  );
+
+  const authorName = usePrompt(
+    "Author full name [Henrotay Mathieu]",
+    "Henrotay Mathieu"
+  );
+
+  const authorEmail = usePrompt(
+    "Author email [mathieu.henrotay@gmail.com]",
+    "mathieu.henrotay@gmail.com"
+  );
+
+  const data = {
+    organizationName,
+    packageName,
+    description,
+    authorName,
+    authorEmail,
+  };
+
+  useDisplayJson({ location, ...data });
+  const sayBye = () => useSentence("Happy coding, see you soon 👋");
+
+  const isConfirmed = useConfirm("Is it correct ? ");
+
+  if (!isConfirmed) {
+    useSentence("Scaffolding was cancelled ❌");
+    return sayBye();
+  }
+
+  const generator = useGenerator(data);
+
+  generator.copy(useStubsPath(), location);
+
+  useSentence("Successfully scaffolded project ✅");
+  sayBye();
 };
 
 export default useScaffolding;
